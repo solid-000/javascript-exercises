bf53a993b2b6f7bdc9e9d5beb3fd42506ec230b2
raj <razbc12@gmail.com>
2024-12-11 21:23:46 +0545

    Complete Assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "";
+    for(let i = string.length-1 ; i>=0 ; i--){
+        result += string.slice(i , i+1);
+    }
+    return result;
 
 };
 

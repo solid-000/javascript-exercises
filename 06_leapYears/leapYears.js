b4683bac34d5678f86393356a82471dadfa70f1c
raj <razbc12@gmail.com>
2024-12-12 17:50:09 +0545

    Complete Assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(date) {
+    if( (date%4===0 && date%100!==0) || date%400 === 0){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line

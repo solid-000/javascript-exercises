c0c1424ea0c2b43f67e6b1969c7c83957de2a565
raj <razbc12@gmail.com>
2024-12-12 16:36:35 +0545

    Complete Assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    for(let i=1; i<arguments.length; i++){
+        for(let j=0; j<arguments[0].length; j++){
+            if(arguments[i] === arguments[0][j]){
+                delete arguments[0][j];
+            }
+        }
+    }
+    arguments[0] = arguments[0].filter((a) => a != undefined);
+    return arguments[0];
 };
 
 // Do not edit below this line

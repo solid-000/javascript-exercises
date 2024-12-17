032b13a835b2bb0dcedc826d1061307f6a350c07
raj <razbc12@gmail.com>
2024-12-17 20:35:19 +0545

    Complete Assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = (Array.from(str)).filter(char => char != " " && char != "." && char != "," && char != "!");
+    let revStr = arr.toReversed().join("");
+    let original = arr.join("");
+    console.log(revStr)
+    console.log(original)
+    return (original.toLowerCase() === revStr.toLowerCase()) ? true : false;
 };
 
 // Do not edit below this line

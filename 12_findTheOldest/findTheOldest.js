bcf88f85a49acfeafd3daf3812bddcfbc65bd55f
raj <razbc12@gmail.com>
2024-12-18 13:12:53 +0545

    Complete Assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let currentYear = new Date().getFullYear();
+    let sortedArray = arr.sort((a, b) => ((b.yearOfDeath||currentYear)-b.yearOfBirth) - ((a.yearOfDeath||currentYear)-a.yearOfBirth));
+    return sortedArray[0];
 };
 
 // Do not edit below this line

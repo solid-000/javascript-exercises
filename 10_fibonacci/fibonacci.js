739b44f6fb9ac9a7b0260b92e0e091b9d5e51b45
raj <razbc12@gmail.com>
2024-12-18 12:39:22 +0545

    Complete Assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0) return "OOPS";
+    let array = [0, 1];
+    for(let i=2; i<=num; i++){
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array[num];
 };
 
 // Do not edit below this line
